0241946c67a2a4ee310c34ff38c90b6bd3830602
Naeem Ahmed <naeem.ahmed@portonics.com>
2022-07-30 13:55:02 +0600

    filtering appointments with url is done

--- src/components/form/AppointmentSelection.js
+++ src/components/form/AppointmentSelection.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { months } from "../../utils";
 import { useDispatch, useSelector } from "react-redux";
 import { popupToggle, setYear, setMonth } from "../../redux/slice/uiSlice";
 import { ADD_APPOINTMENT_POPUP } from "../../constants/uiConstants";
+
+// staic
+const availableYears = [2021, 2020, 2019];
 
 const AppAppointmentSelection = () => {
   const dispatch = useDispatch();
@@ -21,6 +24,26 @@ const AppAppointmentSelection = () => {
     e.preventDefault();
     dispatch(setMonth(e.target.value));
   };
+
+  useEffect(() => {
+    const pathname = window.location.pathname.split("/");
+
+    if (pathname.includes("year")) {
+      const yearPosition = pathname.indexOf("year") + 1;
+      const urlYear = parseInt(pathname[yearPosition]);
+      if (availableYears.includes(urlYear)) {
+        dispatch(setYear(urlYear));
+      }
+    }
+
+    if (pathname.includes("month")) {
+      const yearPosition = pathname.indexOf("month") + 1;
+      const urlMonth = parseInt(pathname[yearPosition]) - 1;
+      if (urlMonth < 12) {
+        dispatch(setMonth(urlMonth));
+      }
+    }
+  }, [window.location.pathname]);
 
   return (
     <div className="selection">
